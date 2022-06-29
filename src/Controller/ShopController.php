d16108277eecbcb312adc79c959471cd168a3b14
thibault roux <roux.thibault@gmail.com>
2022-06-29 19:19:17 +0200

    feat(CRUD-API): Create Shop route added

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -6,6 +6,7 @@ use App\Entity\Shop;
 use App\Repository\ShopRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
@@ -53,6 +54,33 @@ class ShopController extends AbstractController
 		return new JsonResponse(
 			$jsonData,
 			JsonResponse::HTTP_OK,
+			[],
+			true
+		);
+	}
+
+	/**
+	 * @Route("/shop", name="shop_create", methods={"POST"})
+	 */
+	public function createShop(Request $request): JsonResponse
+	{
+		$payload = $request->getContent();
+		if (empty($payload)) {
+			return new JsonResponse(
+				'Error: Empty JSON payload',
+				JsonResponse::HTTP_NOT_FOUND,
+			);
+		}
+
+		$newShop = $this->serializer->deserialize($request->getContent(), Shop::class, 'json');
+
+		$this->shopRepository->add($newShop, $flush = true);
+
+		$jsonData = $this->serializer->serialize($newShop, 'json', ['groups' => ['list_shop']]);
+
+		return new JsonResponse(
+			$jsonData,
+			JsonResponse::HTTP_CREATED,
 			[],
 			true
 		);
